89a2abae8a938d444d0df339e58b28125b868b9c
Hoàng Linh <linhhv.196@gmail.com>
2026-06-26 23:15:36 +0700

    20260626-001

--- src/routes/[[lang]]/+layout.svelte
+++ src/routes/[[lang]]/+layout.svelte
@@ -2,32 +2,32 @@
     import "../../app.css";
     import Header from "$lib/components/Header.svelte";
     import Footer from "$lib/components/Footer.svelte";
-    import { afterNavigate } from '$app/navigation';
-    import { page } from '$app/stores';
-    import { tick } from 'svelte';
+    // import { afterNavigate } from '$app/navigation';
+    // import { page } from '$app/stores';
+    // import { tick } from 'svelte';
 
     let { children } = $props();
 
-    afterNavigate(async () => {
-        if (typeof window === 'undefined') return;
-        if (typeof (window as any).gtag !== 'function') return;
+    // afterNavigate(async () => {
+    //     if (typeof window === 'undefined') return;
+    //     if (typeof (window as any).gtag !== 'function') return;
 
-        await tick(); 
+    //     await tick(); 
 
-        console.log('[GA]', {
-            page_location: $page.url.href,
-            page_path: $page.url.pathname,
-            page_title: document.title,
-            page_referrer: document.referrer
-        });
+    //     console.log('[GA]', {
+    //         page_location: $page.url.href,
+    //         page_path: $page.url.pathname,
+    //         page_title: document.title,
+    //         page_referrer: document.referrer
+    //     });
 
-        (window as any).gtag('event', 'page_view', {
-            page_location: $page.url.href,
-            page_path: $page.url.pathname,
-            page_title: document.title,
-            page_referrer: document.referrer
-        });
-    });
+    //     (window as any).gtag('event', 'page_view', {
+    //         page_location: $page.url.href,
+    //         page_path: $page.url.pathname,
+    //         page_title: document.title,
+    //         page_referrer: document.referrer
+    //     });
+    // });
 </script>
 
 <Header />
