e46c81c70db86a1d8c1a8053a292d452428da361
Hoàng Linh <linhhv.196@gmail.com>
2026-07-09 10:47:15 +0700

    20260709-002

--- src/lib/components/AAdsBanner.svelte
+++ src/lib/components/AAdsBanner.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     // Bạn có thể truyền adUnitId hoặc thay đổi trực tiếp trong iframe
     let { adUnitId = "2447450" } = $props(); // Bạn có thể thay đổi ID này
+
+    let loadAd = $state(false);
+
+    onMount(() => {
+        // Đẩy việc load iframe ra khỏi luồng render chính của trình duyệt
+        // Giúp web mượt mà ngay khi mới mở mà vẫn load được ads (không cần đợi scroll)
+        const timer = setTimeout(() => {
+            loadAd = true;
+        }, 500);
+        return () => clearTimeout(timer);
+    });
 </script>
 
 <div class="aads-banner-container">
-    {#if adUnitId}
+    {#if adUnitId && loadAd}
         <iframe
             data-aa={adUnitId}
             src={`//ad.a-ads.com/${adUnitId}?size=250x250`}
@@ -13,6 +25,9 @@
             title="AAds Banner"
             sandbox="allow-scripts allow-popups allow-popups-to-escape-sandbox"
         ></iframe>
+    {:else}
+        <!-- Giữ sẵn không gian (placeholder) để không bị giật khung hình (Cumulative Layout Shift) -->
+        <div style="width:250px; height:250px;"></div>
     {/if}
 </div>
 
