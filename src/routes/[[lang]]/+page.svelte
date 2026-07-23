535fb471f6520d1f4be56b22259a53e758bdd266
Hoàng Linh <linhhv.196@gmail.com>
2026-07-23 18:26:41 +0700

    20260723-003

--- src/routes/[[lang]]/+page.svelte
+++ src/routes/[[lang]]/+page.svelte
@@ -204,7 +204,7 @@
             {/if}
         </div>
         <div class="sidebar-ad">
-            <Adcash250Banner />
+            <!-- <Adcash250Banner /> -->
             <BannerAds adKey={"85c87dc7bfff537a2e642741cb7ba59a"} reloadAfter={30} />
         </div>
     </aside>
