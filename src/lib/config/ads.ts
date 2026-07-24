13af5744c3dbc5db1d6dad8c83938969f63b16f5
Hoàng Linh <linhhv.196@gmail.com>
2026-07-24 19:01:06 +0700

    20260724-004

--- src/lib/config/ads.ts
+++ src/lib/config/ads.ts
@@ -10,7 +10,7 @@ export const adsConfig = {
     banner_sidebar: {
         id: "85c87dc7bfff537a2e642741cb7ba59a",
         size: "300x250",
-        reload_after: 30
+        reload_after: 20
     },
     adcash_banner_sidebar: {
         id: "11765042",
