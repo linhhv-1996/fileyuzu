01f957386f6107badb34749c0ddc4eefb63e4e15
Hoàng Linh <linhhv.196@gmail.com>
2026-08-04 17:07:26 +0700

    20260804-001-heic

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	worker: {
+		format: 'es'
+	},
 	server: {
 		headers: {
 			'Cross-Origin-Embedder-Policy': 'require-corp',
