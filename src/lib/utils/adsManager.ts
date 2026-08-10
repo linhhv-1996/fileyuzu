84078f1a1a4ba349c629f5ba8f1b741f3c8e3e92
linhhv-1996 <61966209+linhhv-1996@users.noreply.github.com>
2026-08-10 13:29:41 +0700

    Update adsManager.ts

--- src/lib/utils/adsManager.ts
+++ src/lib/utils/adsManager.ts
@@ -129,7 +129,7 @@ class AdsManager {
 
 // Khởi tạo AdsManager quản lý giới hạn CHUNG toàn site
 export const adsManager = new AdsManager({
-    cooldownMinutes: 15,         // Cách <n> phút mới nhảy popup 1 lần bất kể ở màn nào
+    cooldownMinutes: 5,         // Cách <n> phút mới nhảy popup 1 lần bất kể ở màn nào
     defaultProbability: 1,    // <n>% cơ hội nhảy ads (nếu màn không truyền xác suất riêng)
-    maxPerSession: 10           // 1 phiên truy cập ăn tối đa <n> ads tổng cộng
+    maxPerSession: 100           // 1 phiên truy cập ăn tối đa <n> ads tổng cộng
 });
